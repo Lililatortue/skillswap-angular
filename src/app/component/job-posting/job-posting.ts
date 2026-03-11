33a40a158fc777c20d7af89a2558e7a1edea9c2b
Lililatortue <will.descoteaux@gmail.com>
2026-03-11 19:41:56 -0400

    added job posting component

---
+++ src/app/component/job-posting/job-posting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-posting',
+  imports: [],
+  templateUrl: './job-posting.html',
+  styleUrl: './job-posting.css',
+})
+export class JobPosting {
+
+}
